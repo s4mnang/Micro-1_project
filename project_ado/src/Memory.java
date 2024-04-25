56f151ac6e4e7661e016ee8bb3b41f97f2528eab
Francois-Clement Lacolonge <francois-clement.lacolonge@epita.fr>
2024-04-24 15:57:28 -1000

    modif ?

--- project_ado/src/Memory.java
+++ project_ado/src/Memory.java
@@ -1,2 +1,22 @@
 public class Memory {
+    private int[] mem;
+
+    public Memory(int cap) {
+        mem = new int[cap];
+    }
+
+    public int read(int address) {
+        return mem[address];
+    }
+
+    public void write(int address, int value) {
+        mem[address] = value;
+    }
+
+    public void dump() {
+        for (int i = 0; i < mem.length; i++) {
+            System.out.printf("%02X: %02X\n", i, mem[i]);
+        }
+
+    }
 }
--- project_ado/src/Processor.java
+++ project_ado/src/Processor.java
@@ -1,2 +1,21 @@
 public class Processor {
+
+    private int[] proc;
+
+
+    public Processor() {
+        proc = new int[256];
+    }
+
+    public void step() {
+
+    }
+
+    public void dump() {
+        for (int i = 0; i < proc.length; i++) {
+            System.out.printf("%02X: %02X\n", i, proc[i]);
+        }
+
+    }
+
 }
