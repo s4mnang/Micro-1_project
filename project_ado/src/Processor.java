d7b2020e7bce304b687545796b615136cca7644c
Francois-Clement Lacolonge <francois-clement.lacolonge@epita.fr>
2024-04-24 16:15:04 -1000

    step 1 done

--- project_ado/src/Processor.java
+++ project_ado/src/Processor.java
@@ -1,14 +1,18 @@
 public class Processor {
 
     private int[] proc;
-
+    private Memory mem;
 
     public Processor() {
         proc = new int[256];
     }
 
-    public void step() {
-
+    public boolean step() {
+        boolean halt = true;
+        if (proc[0] == 0) { // proc[0]  ?????????
+            halt = false;
+        }
+        return halt;
     }
 
     public void dump() {
@@ -16,6 +20,14 @@ public class Processor {
             System.out.printf("%02X: %02X\n", i, proc[i]);
         }
 
+    }
+
+    public void setMemory( Memory mem) {
+        this.mem = mem;
+    }
+
+    public void setPC(int mem) {
+        proc[0] = mem;
     }
 
 }
